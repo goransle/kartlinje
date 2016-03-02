798dda4cb625971c3f9df6bcb2285f0ef8518b66
goransle <goransle@gmail.com>
2016-03-02 13:33:34 +0100

    now with more structure

--- script.js
+++ script.js
@@ -6,81 +6,30 @@ var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 12, attribution: osmAttr
 //kartoppsett
 map.setView(new L.LatLng(60.683629, 5.030783),3);
 map.addLayer(osm);
-//markers
-var radoy = L.marker([60.683629, 5.030783]).addTo(map);
-var bergen = L.marker([60.401430, 5.314131]).addTo(map);
-var havet = L.marker([50.307612, -25.324032]).addTo(map);
-var ellis = L.marker([40.699483, -74.039536]).addTo(map);
-var brooklyn = L.marker([40.659225, -73.951874]).addTo(map);
-var minnesota = L.marker([44.848076, -94.086414]).addTo(map);
 
-//custom ikon
-var boatIcon = L.icon({
-    iconUrl: 'JBFord.png',
-    shadowUrl: 'JBFord.png',
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+var episoder = [];
+var markers = [];
+
+//legge til episoder
+episoder.push(["Episode 1", [60.683629, 5.030783], "test.html"]);
+episoder.push(["Episode 2", [60.683629, 7.030783], "test2.html"]);
+
+//iterere over episoder
+episoder.forEach(function (item, index, array) {
+  console.log(item[1]);
+  markers[index] = L.marker(item[1]).addTo(map);
+  $("nav").append("<button class='epButts' id='" + index + "'>" + item[0] + "</button>");
+  $("article").append("<section id='"+ index +"'></section>");
+  $("section#"+index).load("./"+item[2]);
 });
 
-$(radoy).click(function() {
-   $("#radoy").show();
-   bergen.addTo(map);
+//knappar
+$(".epButts").click(function() {
+    map.panTo(markers[this.id].getLatLng());
+    $("section").hide();
+    $("article").show();
+    $("section#"+this.id).show();
 });
-$(bergen).click(function() {
-   $("#radoy").show();
-   havet.addTo(map);
-});
-$(havet).click(function() {
-   $("#radoy").show();
-   ellis.addTo(map);
-});
-$(ellis).click(function() {
-   $("#radoy").show();
-   brooklyn.addTo(map);
-});
-$(brooklyn).click(function() {
-   $("#radoy").show();
-   minnesota.addTo(map);
-});
-$(minnesota).click(function() {
-   $("#radoy").show();
-});
-
 $(".lukk").click(function() {
-   $(".info").hide();
-   $("section").hide();
-});
-$("#1").click(function() {
-  $("section").hide();
-   $(".info").show();
-   $("#radoy").show();
-   map.panTo(radoy.getLatLng());
-   map.setZoom(8);
-});
-$("#2").click(function() {
-  $("section").hide();
-   $(".info").show();
-   $("#bergen").show();
-   map.panTo(bergen.getLatLng());
-   map.setZoom(5);
-});
-$("#3").click(function() {
-  $("section").hide();
-   $(".info").show();
-   $("#havet").show();
-   map.panTo(havet.getLatLng(), {animate:true});
-});
-$("#4").click(function() {
-  $("section").hide();
-   $(".info").show();
-   $("#ellis").show();
-   map.panTo(ellis.getLatLng());
-});
-$("#5").click(function() {
-  $("section").hide();
-   $(".info").show();
-   map.panTo(minnesota.getLatLng());
+    $("article").hide();
 });
